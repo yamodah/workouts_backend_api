d9709941d794f119fba66d65fcfc4907eec03bd6
yamodah <yassinedahlek@gmail.com>
2021-12-23 18:18:44 -0500

    app.js built out and connected

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
-
+const wokroutsRouter = require("./workouts/workouts.router")
+const notFound = require("./errors/notFound")
+const errorHandler = require("./errors/errorHandler")
 
 app.use(express.json())
 app.use(cors())
+app.use("/workouts", wokroutsRouter)
 app.get("/",(req,res,next)=>{
-    res.send("welcome to your workouts data")
+    res.send("welcome to the workouts api. please navigate to '/workouts'.")
 })
+app.use(notFound)
+app.use(errorHandler)
 module.exports = app
