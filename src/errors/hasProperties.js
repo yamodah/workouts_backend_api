4bbf29625a7b340ff84c1763ec5223d43b39b027
yamodah <yassinedahlek@gmail.com>
2021-12-23 17:34:19 -0500

    added gerneric error folder

---
+++ src/errors/hasProperties.js
@@ -0,0 +1,27 @@
+/**
+ * Creates a middleware function that validates that req.body.data has the specified non-falsey properties.
+ * @param properties
+ *  one or more property name strings.
+ * @returns {function(*, *, *): void}
+ *    a middleware function that validates that req.body.data has the specified non-falsey properties.
+ */
+function hasProperties(...properties) {
+  return function (req, res, next) {
+    const data = req.body
+    try {
+      properties.forEach((property) => {
+        
+        if (!data[property]) {
+          const error = new Error(`A '${property}' property is required.`);
+          error.status = 400;
+          throw error;
+        }
+      });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+module.exports = hasProperties;
