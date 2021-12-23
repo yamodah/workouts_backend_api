f7771afa500a7ab7ad61e5ade74a45a827e0c8a0
yamodah <yassinedahlek@gmail.com>
2021-12-23 16:05:54 -0500

    more infrastructure built out -- migration built out

--- src/db/migrations/20211223184603_createWorkoutsTable.js
+++ src/db/migrations/20211223184603_createWorkoutsTable.js
@@ -1,7 +1,23 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable("workouts",(table)=>{
-
+      table.increments("id").primary()
+      table.date("date")
+      table.string("type")
+      table.integer("duration")
+      table.string("goal")
+      table.integer("pre")
+      table.integer("post")
+      table.integer("intensity")
+      table.text("performance")
+      table.text("pain")
+      table.text("body_parts")
+      table.string("sport")
+      table.string("venue")
+      table.boolean("competitive")
+      table.integer("distance")
+      table.string("cardio_type")
+      table.string("speed")
   })
 };
 
