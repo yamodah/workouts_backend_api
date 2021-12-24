f8e6c8b68153ccf13915fa685f21f9d981cde6d4
yamodah <yassinedahlek@gmail.com>
2021-12-23 19:33:48 -0500

    added basic seed data to help with builing

--- src/db/seeds/01_workouts.js
+++ src/db/seeds/01_workouts.js
@@ -5,9 +5,47 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('workouts').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {
+          type:"resistance",
+          date:"2021-02-24",
+          duration:90,
+          goal:"general fitness, get back in the gym",
+          pre:5,
+          post:7,
+          intensity:6,
+          performance:"solid workout -- nothing special but got to the gym and moved some weight",
+          pain:"no major pain -- some ankle soreness -- maybe a tweak in the left shoulder",
+          body_parts:"full-body",
+        },
+        {
+          type:"sports",
+          date:"2021-03-19",
+          duration:180,
+          goal:"work up a sweat and get some good touches",
+          pre:7,
+          post:9,
+          intensity:8,
+          performance:"good touches overall good play",
+          pain:"bottom of foot soreness, left ankle swollen, slight tightness in back",
+          sport:"soccer",
+          venue:"indoor",
+          competitive:false,
+        },
+        {
+          type:"cardio",
+          date:"2021-07-22",
+          duration:30,
+          goal:"work up a sweat and get some good touches",
+          pre:4,
+          post:7,
+          intensity:6,
+          performance:"great to be back doing cardio -- need a few to get in rhythm",
+          pain:"lung fatigue -- hamstring tightness -- left ankle clicking",
+          distance:3,
+          cardio_type:"bike",
+          speed:"95 rpm",
+
+        },
       ]);
     });
 };
